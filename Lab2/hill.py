d4a319bc6c407703795301135d6f8d6353a4bf1b
aditya1904 <adityamalu1@gmail.com>
2017-08-18 16:29:53 +0530

    fixed buggy encryption

--- Lab2/hill.py
+++ Lab2/hill.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
-
 from string import punctuation
 import sys
-
-alphabets = 'abcdefghijklmnopqrstuvwxyz'
 
 def remove_punctuation(message):
 	message = message.translate(None, punctuation); #removing !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~
@@ -11,20 +8,13 @@ def remove_punctuation(message):
 	return message.lower()
 
 def padthemessage(message):
-	if(len(message)%5 == 0):
+	if(len(message)%3 == 0):
 		return message
 	else:
-		short = 5 - len(message)%5
+		short = 3 - len(message)%3
 		for i in range(short):
 			message += 'x'				#appending x to make message length a multiple of 5
 		return message
-
-def padthekey(key):
-	for alphabet in alphabets:
-		if alphabet not in key:
-			key += alphabet
-		if(len(key) == 25):
-			return key
 
 def display_format(message):
 	'''	Print the final encoded message in blocks of five letters and ten blocks per line.
@@ -42,52 +32,41 @@ def display_format(message):
 			enc = enc + block + " "
 	return enc
 
-def matrix_multiply(message_matrix, key_matrix):
-	rows = len(message_matrix)
-	cols = len(key_matrix[0])
-	cipher_matrix = [[0]]*rows
-	for i in range(rows):
-		row = []
-		for j in range(cols):
-			res = 0
-			for k in range(len(key_matrix)):
-				res += (message_matrix[i][k] * key_matrix[k][j])
-			row.append(chr(ord('a') + res%26))
-		cipher_matrix[i] = row
-	cipher_matrix = [list(x) for x in zip(*cipher_matrix)] ## transpose of a matrix
-	return cipher_matrix
-
-def generate_matrix(content, rows, cols, type):
-	if(type == 'key'):
-		matrix = [[0]]*rows	 #initializing a matrix with zeros
-		k = 0
+def matrix_multiply(matrix_1, matrix_2):
+	if(len(matrix_1[0]) == len(matrix_2)): ##Condition for matrix multiplication.
+		rows = len(matrix_1)
+		cols = len(matrix_2[0])
+		cipher_matrix = [[0]]*rows
 		for i in range(rows):
 			row = []
 			for j in range(cols):
-				row.append(ord(content[k])-ord('a'))
-				k+=1
-			matrix[i] = row
-		return matrix
-	if(type == 'message'):
-		### for plain_text we have to fill the matrix column wise, so first we fill it row wise and then take transpose.
-		matrix = [[0]]*cols	 #initializing a matrix with zeros
-		k = 0
-		for i in range(cols):
-			row = []
-			for j in range(rows):
-				row.append(ord(content[k])-ord('a'))
-				k+=1
-			matrix[i] = row
-		matrix = [list(x) for x in zip(*matrix)] ## transpose of a matrix
-		return matrix
+				res = 0
+				for k in range(len(matrix_2)):
+					res += (matrix_1[i][k] * matrix_2[k][j])
+				row.append(chr(ord('a') + res%26))
+			cipher_matrix[i] = row
+		cipher_matrix = [list(x) for x in zip(*cipher_matrix)] ## transpose of a matrix
+		return cipher_matrix
+	else:
+		print "Matrices cannnot be multiplied..."
+		sys.exit(0);
+
+def generate_matrix(content, rows, cols):
+	matrix = [[0]]*rows	 #initializing a matrix with zeros
+	k = 0
+	for i in range(rows):
+		row = []
+		for j in range(cols):
+			row.append(ord(content[k])-ord('a'))
+			k+=1
+		matrix[i] = row
+	return matrix
 
 def encrypt(message, key):
-	key = padthekey(key)
-	key_matrix = generate_matrix(key, 5, 5, 'key')
-	message = remove_punctuation(message)
-	message = padthemessage(message)
-	message_matrix = generate_matrix(message, len(message)/5 , 5, 'message')
-	cipher_matrix = matrix_multiply(message_matrix, key_matrix)
+	key_matrix = generate_matrix(key, 3, 3)
+	message_matrix = generate_matrix(message, len(message)/3 , 3)
+	message_matrix = [list(x) for x in zip(*message_matrix)] ## transpose of a matrix
+	cipher_matrix = matrix_multiply(key_matrix, message_matrix)
 	ciphertext = ''.join([ch for row in cipher_matrix for ch in row])
 	ciphertext = display_format(ciphertext)
 	return ciphertext
@@ -96,8 +75,11 @@ def main():
 	try:
 		case = int(raw_input("1 to encrypt plain_text\n2 to decrypt ciphertext\nEnter option : "))
 		if(case == 1):
-			k = raw_input("Enter key(max 9 chars): ")
-			message = raw_input("Message[Plain Text]: ")
+			k = raw_input("Enter key (exactly 9 chars): ")
+			if(len(k) != 9):
+				print("Enter key of size 9 only...")
+				sys.exit(0)
+			message = padthemessage(remove_punctuation(raw_input("Message[Plain Text]: ")))
 			encrypted_message = encrypt(message, k)
 			print "Encrypted Message..."
 			print encrypted_message
